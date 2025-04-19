c1fc3a787b29b28f98db8561c74b7b1207087845
ukryzh <kryzhulyana@gmail.com>
2025-04-19 12:02:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ function openModal(src) {
   const modal = document.getElementById('modal');
   const modalImg = document.getElementById('modalImg');
   modalImg.src = src;
-  modal.style.display = 'block';
+  modal.style.display = 'flex';
 }
 
 function closeModal() {
