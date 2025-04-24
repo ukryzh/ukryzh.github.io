85e6d51346df5859fcb0cbf3e40c5f07e735fa9b
Ulyana Kryzh <kryzhulyana@gmail.com>
2025-04-24 09:15:30 +0300

    Revert "Update script.js слайды"
    This reverts commit a5a9a0456e048222986acf6d6bd304233b419856.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let resumeContent = contentContainer.innerHTML;
 
 let activeCase = null;
 let activeButton = null;
-let currentSlideIndex = 0;
 
 function loadCase(caseFile, clickedBtn = null) {
   if (!caseFile) return;
